a955f122ce3c43db5aa877b89215af5333f1ce10
hugocanovas <canovashugo83@gmail.com>
2023-01-09 19:16:22 +0100

    rien a ajouter

--- User.php
+++ User.php
@@ -1,56 +1,123 @@
 <?php
-
+    
     try
     {
         $bdd = new mysqli('localhost', 'root', '', 'classes');
-    }catch(Exception $e)
+        echo 'connexion établie';
+    }
+    catch(Exception $e)
     {
         die ('Erreur : '.$e->getMessage());
     }
-?>
 
-<?php
     class User
     {
-
+        
         private $id;
         public $login;
-        private $password;
+        public $password;
         public $email;
         public $firstname;
         public $lastname;
 
-        public function __construct()
+        public function __construct($login, $password, $email, $firstname, $lastname)
         {
-            $this->id;
-            $this->login;
-            $this->password;
-            $this->email;
-            $this->firstname;
-            $this->lastname;
+            $this->login = $login;
+            $this->password = $password;
+            $this->email = $email;
+            $this->firstname = $firstname;
+            $this->lastname = $lastname;
+
+        }
+
+        public function getId()
+        {
+            return $this->id;
+        }
+
+        public function getLogin()
+        {
+            return $this->login;
+        }
+
+        public function getPassword()
+        {
+            return $this->password;
+        }
+
+        public function getEmail()
+        {
+            return $this->email;
+        }
+
+        public function getFirstname()
+        {
+            return $this->firstname;
+        }
+
+        public function getLastname()
+        {
+            return $this->lastname;
         }
 
     }
 
-    // VISUALISATION DES USERS DANS UN TEABLEAU
+?>
 
-    // $req = ("SELECT * FROM utilisateurs");
-    // $res = $bdd->query($req);
+<!-- <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="index.php" method="post">
+        
+        <label for="login">Pseudo : </label>
+        <input type="text" name="login">
 
-    // echo "<table>";
-    // while($data = mysqli_fetch_array($res)){
-    //     echo "<tr><td>".$data['login']."</td><td>".$data['email']."</td></tr>";
-    // }
-    // echo "</table>";
+        <label for="password">Mot de passe : </label>
+        <input type="password" name="password">
 
-    // INSERTION USER BDD 
+        <label for="email">Email : </label>
+        <input type="email" name="email">
 
-    // $req = "INSERT INTO utilisateurs (login, password, email, firstname, lastname) VALUES ('test5', 'test', 'test5@gmail.com', 'test5', 'test5')";
-    // $res = $bdd->query($req);
+        <label for="firstname">Nom : </label>
+        <input type="text" name="firstname">
 
-    // DELETE USER BDD
+        <label for="lastname">Prénom : </label>
+        <input type="text" name="lastname">
 
-    // $req = "DELETE FROM utilisateurs WHERE id = 5";
-    // $res = $bdd->query($req);
+        <input type="submit" value="Inscrription">
+
+    </form>
+</body>
+</html> -->
+
+<?php
+
+// if($_POST){
+//     if($_POST['login'] != ''){
+        
+//         $req = $bdd->prepare("INSERT INTO utilisateurs (login, password, email, firstname, lastname) VALUES (?, ?, ?, ?, ?)");
+
+//         $login = $_POST['login'];
+//         $password = $_POST['password'];
+//         $email = $_POST['email'];
+//         $firstname = $_POST['firstname'];
+//         $lastname = $_POST['lastname'];
+        
+//         $req->bind_param("sssss", $login, $password, $email, $firstname, $lastname);
+//         $req->execute();
+
+//     }
+// }
+
+    
+
+    
+    
 
 ?>
