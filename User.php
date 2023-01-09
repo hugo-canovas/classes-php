63fe84e45d7b76690e985cf7cc9bf7fc33d51daa
hugo-canovas <canovashugo83@gmail.com>
2023-01-09 12:04:29 +0100

    test

--- User.php
+++ User.php
@@ -7,6 +7,7 @@
     {
         die('Erreur : '.$e->getMessage());
     }
+    
 ?>
 
 <form action="User.php" method="post">
@@ -23,73 +24,14 @@
     <input type="submit" value="Inscription">
 </form>
 
-<?php
-
-    $req = $bdd->prepare("INSERT into utilisateur VALUES (login, password, email, firstname, lastname)");
-    $req->execute(array(
-        'login' => $_POST['login'],
-        'password' => $_POST['password'],
-        'email' => $_POST['email'],
-        'firstname' => $_POST['name'],
-        'lastname' =>$_POST['prenom']
-    ))
-
-?>
-
 
 <?php
 
-    class User
-    {
+   
 
-        private $id;
-        public $login;
-        private $password;
-        public $email;
-        public $firstname;
-        public $lastname;
-        
-        public function __construct($login, $password, $email, $firstname, $lastname)
-        {
-            $this->login = $login;
-            $this->password = $password;
-            $this->email = $email;
-            $this->firstname = $firstname;
-            $this->lastname = $lastname;
-        }
+    $req = mysqli_prepare($bdd, "INSERT INTO utilisateurs (login, password, email, firstname, lastname) VALUES (?, ?, ?, ?, ?)");
 
-        public function id()
-        {
-            return $this->id;
-        }
 
-        public function login()
-        {
-            return $this->login;
-        }
-
-        public function password()
-        {
-            return $this->password;
-        }
-
-        public function email()
-        {
-            return $this->email;
-        }
-
-        public function firstname()
-        {
-            return $this->firstname;
-        }
-
-        public function lastname()
-        {
-            return $this->lastname;
-        }
-        
-
-    }
 
     
     
